42aec623817b14cfdf00504a4b5feb7705bc8b2e
Farhan Chonari <farhanchonari@gmail.com>
2023-05-01 22:12:02 +0530

    navbar edited

--- script.js
+++ script.js
@@ -14,6 +14,37 @@ const formOpenBtn = document.querySelector("#form-open"),
 formOpenBtn.addEventListener("click", () => home.classList.add("show"));
 formCloseBtn.addEventListener("click", () => home.classList.remove("show"));
 
+
+
+document.addEventListener("DOMContentLoaded", function(){
+
+  el_autohide = document.querySelector('.autohide');
+  
+  // add padding-top to bady (if necessary)
+  navbar_height = document.querySelector('.navbar').offsetHeight;
+  document.body.style.paddingTop = navbar_height + 'px';
+
+  if(el_autohide){
+    var last_scroll_top = 0;
+    window.addEventListener('scroll', function() {
+          let scroll_top = window.scrollY;
+         if(scroll_top < last_scroll_top) {
+              el_autohide.classList.remove('scrolled-down');
+              el_autohide.classList.add('scrolled-up');
+          }
+          else {
+              el_autohide.classList.remove('scrolled-up');
+              el_autohide.classList.add('scrolled-down');
+          }
+          last_scroll_top = scroll_top;
+    }); 
+    // window.addEventListener
+  }
+  // if
+
+});
+
+
 pwShowHide.forEach((icon) => {
   icon.addEventListener("click", () => {
     let getPwInput = icon.parentElement.querySelector("input");
@@ -62,7 +93,7 @@ function showPopUp(day) {
     popUp.innerHTML = `<div>
                         <h2>${months[currMonth]} ${day}, ${currYear}</h2>
                        <p>Today is a special day!</p>
-                       <img src="im.jpeg">
+                      <img src="im.jpeg">
                        <p>Happy birthday!</p>
                        <button class="close-btn">Close</button>
                           <div/>`;
@@ -70,7 +101,6 @@ function showPopUp(day) {
     popUp.querySelector(".close-btn").addEventListener("click", () => {
         popUp.remove();
     });
-    
     document.body.appendChild(popUp);
 }
 const renderCalendar = () => {
@@ -122,3 +152,21 @@ prevNextIcon.forEach(icon => {
     });
 });
 
+for(let i=1;i<6;i++){
+  const rangeThumb = document.getElementById(`range-thumb${i}`),
+        rangeNumber = document.getElementById(`range-number${i}`),
+        rangeLine = document.getElementById(`range-line${i}`),
+        rangeInput = document.getElementById(`range-input${i}`)
+  
+  const rangeInputSlider = () =>{
+     rangeNumber.textContent = rangeInput.value
+     const thumbPosition = (rangeInput.value / rangeInput.max),
+     space = rangeInput.offsetWidth - rangeThumb.offsetWidth
+     rangeThumb.style.left = (thumbPosition * space) + 'px'
+     rangeLine.style.width = rangeInput.value + '%'
+     rangeInput.addEventListener('input', rangeInputSlider)
+  }
+  
+  rangeInputSlider()
+  }
+  
