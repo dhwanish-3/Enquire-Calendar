167c2b473da441f5b52c8f8ac98052faeff91bca
dhwanish-3 <108540722+dhwanish-3@users.noreply.github.com>
2023-06-23 20:23:20 +0530

    modified:   admin/script.js

---
+++ admin/login.php
@@ -0,0 +1,19 @@
+<?php
+session_start();
+require '../backup/confidential.php';
+if(isset($_POST['submit'])){
+	$email = $_POST['email'];
+	$password = $_POST['password'];
+    if($email==$adminEmail && $password==$adminPassword){
+        $_SESSION['email']=$adminEmail;
+        $_SESSION['name']=$adminName;
+        header("Location: index.php?login=success");
+    }else{
+        $_SESSION['login-msg']="Please use admin credentials";
+        header("Location: index.php?login=failure");
+    }
+}else{
+    echo "Form not submitted";
+}
+
+?>
